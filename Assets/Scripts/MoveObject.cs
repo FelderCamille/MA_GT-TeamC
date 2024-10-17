1855beaafcf21eea93682d2c0ea68ab58997bd03
Camille Felder <felder.camille@outlook.com>
2024-10-17 13:17:55 +0200

    chore: add robot and mine as cubes to simulate a collision

---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    [SerializeField] public float speed = 5f;
+
+    private void Update()
+    {
+        // Move to right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+        }
+        // Move to left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
+        }
+        // Move up
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0f, 0f, speed * Time.deltaTime);
+        }
+        // Move down
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= new Vector3(0f, 0f, speed * Time.deltaTime);
+        }
+    }
+}

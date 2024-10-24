e040fccfddfef3d840afeea134a7b975d054e084
VoelkyD <yoyoku68@gmail.com>
2024-10-24 23:01:23 +0200

    wasd command

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -7,22 +7,22 @@ public class MoveObject : MonoBehaviour
     private void Update()
     {
         // Move to right
-        if (Input.GetKey(KeyCode.RightArrow))
+        if ((Input.GetKey(KeyCode.RightArrow) || (Input.GetKey(KeyCode.D))))
         {
             transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
         }
         // Move to left
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if ((Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.A))))
         {
             transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
         }
         // Move up
-        if (Input.GetKey(KeyCode.UpArrow))
+        if ((Input.GetKey(KeyCode.UpArrow) || (Input.GetKey(KeyCode.W))))
         {
             transform.position += new Vector3(0f, 0f, speed * Time.deltaTime);
         }
         // Move down
-        if (Input.GetKey(KeyCode.DownArrow))
+        if ((Input.GetKey(KeyCode.DownArrow) || (Input.GetKey(KeyCode.S))))
         {
             transform.position -= new Vector3(0f, 0f, speed * Time.deltaTime);
         }

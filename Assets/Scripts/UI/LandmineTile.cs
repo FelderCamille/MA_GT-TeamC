50008c4380a63f648ceb7e81609324d195ece6c6
Camille Felder <camille.felder@hes-so.ch>
2024-11-27 18:35:06 +0100

    chore: show the landmine and hide it using a fading effect

--- Assets/Scripts/UI/LandmineTile.cs
+++ Assets/Scripts/UI/LandmineTile.cs
@@ -1,20 +1,58 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace UI
 {
     public class LandmineTile : Tile
     {
+        private const int ShowTimeInSec = 1;
+        private const float ShowAlpha = 0.7f;
 
         public GameObject landmine;
+        private Material _material;
+        private bool _isShown;
+        
+
+        private void Awake()
+        {
+            _material = landmine.GetComponent<Renderer>().material;
+            Hide();
+        }
 
         public void Show()
         {
-            if (!landmine.activeSelf) landmine.SetActive(true);
+            if (_isShown) return;
+            _isShown = true;
+            ChangeAlpha(ShowAlpha);
+            StartCoroutine(FadeAfterSeconds(ShowTimeInSec));
         }
 
-        public void Hide()
+        private void Hide()
         {
-            if (landmine.activeSelf) landmine.SetActive(false);
+            ChangeAlpha(0f);
+        }
+        
+        private IEnumerator FadeAfterSeconds(float seconds)
+        {
+            // Wait for the given seconds
+            yield return new WaitForSeconds(seconds);
+            // Decrease linearly the alpha value to 0
+            for (var i = ShowAlpha; i >= 0; i -= 0.01f)
+            {
+                ChangeAlpha(i);
+                yield return new WaitForSeconds(0.01f);
+            }
+            // Enable to show the landmine again
+            _isShown = false;
+        }
+        
+        private void ChangeAlpha(float alphaVal)
+        {
+            var oldColor = _material.color;
+            var newColor = new Color(oldColor.r, oldColor.g, oldColor.b, alphaVal);
+            _material.SetColor("_Color", newColor);
+
         }
     }
 }

058584412037d7c7fc7afca717cdfdacfef08e50
Camille Felder <camille.felder@hes-so.ch>
2024-12-06 17:32:47 +0100

    fix: fix store overlay opening

--- Assets/Scripts/Controllers/TentController.cs
+++ Assets/Scripts/Controllers/TentController.cs
@@ -1,4 +1,3 @@
-using UI;
 using UnityEngine;
 
 namespace Controllers
@@ -8,7 +7,6 @@ namespace Controllers
         private const float StoreDistance = Constants.GameSettings.NumberOfTileOpenStore + .5f; // On z axis
         public const int TentLength = 4;
         
-        private Tile _tile;
         private RobotController _robot;
         private StoreController _store;
         private float _tentXPosition;
@@ -17,11 +15,10 @@ namespace Controllers
 
         private void Start()
         {
-            _tile = GetComponent<Tile>();
             _robot = FindObjectOfType<RobotController>();
-            _tentXPosition = transform.position.x + _tile.width / 2;
-            _tentZ1Position = transform.position.z - _tile.depth / 2;
-            _tentZ2Position = transform.position.z + _tile.depth / 2;
+            _tentXPosition = transform.position.x + TentLength - 1;
+            _tentZ1Position = transform.position.z - TentLength / 2;
+            _tentZ2Position = transform.position.z + TentLength / 2 - 1;
             _store = FindObjectOfType<StoreController>(includeInactive: true);
         }
 
